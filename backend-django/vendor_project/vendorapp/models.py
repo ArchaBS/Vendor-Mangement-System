b7b7065bd5339838617c5a1be6bc3a1d20022152
Archa <archagit@gmail.com>
2026-03-13 16:16:56 +0530

    Add Product model with vendor foreign key

---
+++ backend-django/vendor_project/vendorapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    vendor = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    image_url = models.URLField(max_length=500, blank=True, null=True)  # ✅ added
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product_name
