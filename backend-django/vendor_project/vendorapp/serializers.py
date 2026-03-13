b8c99c7174dce9cf143a401a1b9a15655c48913f
Archa <archagit@gmail.com>
2026-03-13 16:20:28 +0530

    Add Product and Register serializers

---
+++ backend-django/vendor_project/vendorapp/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Product
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+        read_only_fields = ['vendor']
